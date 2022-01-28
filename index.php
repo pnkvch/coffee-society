750d72a26de342f03334710aea1b5814540d2510
pnkvch <pinkevychart@gmail.com>
2022-01-28 18:51:40 +0100

    feat: introduced docket to project

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+require 'Router.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url( $path, PHP_URL_PATH);
+
+Router::get('', 'DefaultController');
+Router::get('projects', 'DefaultController');
+
+Router::run($path);
