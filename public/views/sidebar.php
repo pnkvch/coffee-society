d7e775ad3ce665748947810c3baff9b64cb13c57
pnkvch <pinkevychart@gmail.com>
2022-01-30 17:14:53 +0100

    added active item to sidebar

--- public/views/sidebar.php
+++ public/views/sidebar.php
@@ -3,16 +3,15 @@
     <div class="upper-sidebar">
       <img class="logo-sidebar" src="public/images/logo.svg" />
       <div class="links">
-        <a>Feed</a>
-        <a>Coffee Recipies</a>
-        <a>Explore</a>
-        <a>People</a>
+        <a href="/feed" class="active">Feed</a>
+        <a href="/recipies">Coffee Recipies</a>
+        <a href="/explore">Explore</a>
+        <a href="/people">People</a>
       </div>
     </div>
     <div class="lower-sidebar">
       <button class="button">Publish a post</button>
-      <a>My Blog</a>
-      <a>Settings</a>
+      <a href="/blog">My Blog</a>
     </div>
   </aside>
 </div>
