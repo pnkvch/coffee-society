4884244a396b8acd760770864b66963e5352c717
pnkvch <pinkevychart@gmail.com>
2022-02-01 15:59:59 +0100

    fix styling

--- public/views/recipie.php
+++ public/views/recipie.php
@@ -9,6 +9,7 @@
   <?php $this->render('sidebar');?>
   <section class="page-container">
     <h3 class="heading-title"><?= $recipie->getTitle();?></h3>
+    <span class="subtitle"><?= $recipie->getSubtitle();?></span>
     <div class="cards-wrapper">
       <p>
         <?= $recipie->getContent();?>
@@ -16,7 +17,7 @@
     </div>
   </section>
   <div class="article-image-container">
-    <!-- <img class="article-image" src="public/images/filter.jpeg" /> -->
+    <img class="article-image" src="/public/images/filter.jpeg" />
   </div>
   </div>
   </body>
